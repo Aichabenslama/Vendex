f422d7dcc6c53a6713570aa93df53ff925e23752
Aicha ben slama <aichabenslama152@gmail.com>
2026-03-06 21:28:58 +0000

    implement login and register page with authContext integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { AuthProvider } from './context/AuthContext'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
       <Header/>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+
+
+      </Routes>
       </BrowserRouter>
     </AuthProvider>
   )
